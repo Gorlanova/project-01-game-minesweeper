fad4ff3e6a6ecc9355de69d57c9d6de46d79d008
Nina Gautreau <nina.gautreau@gmail.com>
2020-01-23 10:41:44 +0100

    Clear empty cells on click

--- main.js
+++ main.js
@@ -64,15 +64,6 @@ window.onload = () => {
 };
 
 // ================
-// - TO DO LIST
-// ================
-// Click = Check if mine + Reveal tile + Check if over
-// Mine = GAME OVER
-// Number = Continue (expand if Clear)
-// WIN = POP UP (+noise +animation ?)
-// LOSE = POP UP (+noise +animation ?)
-
-// ================
 // - SOUND SPECS
 // ================
 const music = document.getElementById("music");
@@ -272,6 +263,71 @@ function checkOver(cell) {
 // ================
 // - REVEAL CELL
 // ================
+function uncoverNeighbours(cell){
+    let cells = document.querySelectorAll(".cell");
+    let index = [...cells].indexOf(cell);
+
+    let cellNW = cells.item(index-11);
+    let cellNN = cells.item(index-10);
+    let cellNE = cells.item(index-9);
+    let cellWW = cells.item(index-1);
+    let cellEE = cells.item(index+1);
+    let cellSW = cells.item(index+9);
+    let cellSS = cells.item(index+10);
+    let cellSE = cells.item(index+11);
+
+    if (index===0){
+        cellEE.className==="cell cell-back" && revealCell(cellEE);
+        cellSS.className==="cell cell-back" && revealCell(cellSS); 
+        cellSE.className==="cell cell-back" && revealCell(cellSE);
+    } else if (index>0 && index <9){
+        cellWW.className==="cell cell-back" && revealCell(cellWW); 
+        cellEE.className==="cell cell-back" && revealCell(cellEE); 
+        cellSW.className==="cell cell-back" && revealCell(cellSW); 
+        cellSS.className==="cell cell-back" && revealCell(cellSS); 
+        cellSE.className==="cell cell-back" && revealCell(cellSE);
+    } else if (index===9){
+        cellWW.className==="cell cell-back" && revealCell(cellWW);
+        cellSW.className==="cell cell-back" && revealCell(cellSW); 
+        cellSS.className==="cell cell-back" && revealCell(cellSS);
+    } else if (index===10 || index===20 || index===30 || index===40 || index===50 || index===60 || index===70 || index===80){
+        cellNN.className==="cell cell-back" && revealCell(cellNN); 
+        cellNE.className==="cell cell-back" && revealCell(cellNE);
+        cellEE.className==="cell cell-back" && revealCell(cellEE);
+        cellSS.className==="cell cell-back" && revealCell(cellSS); 
+        cellSE.className==="cell cell-back" && revealCell(cellSE);
+    } else if (index===19 || index===29 || index===39 || index===49|| index===59 || index===69 || index===79 || index===89){
+        cellNW.className==="cell cell-back" && revealCell(cellNW);
+        cellNN.className==="cell cell-back" && revealCell(cellNN);
+        cellWW.className==="cell cell-back" && revealCell(cellWW);
+        cellSW.className==="cell cell-back" && revealCell(cellSW); 
+        cellSS.className==="cell cell-back" && revealCell(cellSS);
+    } else if (index===90){
+        cellNN.className==="cell cell-back" && revealCell(cellNN); 
+        cellNE.className==="cell cell-back" && revealCell(cellNE);
+        cellEE.className==="cell cell-back" && revealCell(cellEE);
+    } else if (index>90 && index<99){
+        cellNW.className==="cell cell-back" && revealCell(cellNW);
+        cellNN.className==="cell cell-back" && revealCell(cellNN); 
+        cellNE.className==="cell cell-back" && revealCell(cellNE); 
+        cellWW.className==="cell cell-back" && revealCell(cellWW); 
+        cellEE.className==="cell cell-back" && revealCell(cellEE);
+    } else if (index===99){
+        cellNW.className==="cell cell-back" && revealCell(cellNW);
+        cellNN.className==="cell cell-back" && revealCell(cellNN);
+        cellWW.className==="cell cell-back" && revealCell(cellWW);
+    } else {
+        cellNW.className==="cell cell-back" && revealCell(cellNW);
+        cellNN.className==="cell cell-back" && revealCell(cellNN); 
+        cellNE.className==="cell cell-back" && revealCell(cellNE); 
+        cellWW.className==="cell cell-back" && revealCell(cellWW); 
+        cellEE.className==="cell cell-back" && revealCell(cellEE); 
+        cellSW.className==="cell cell-back" && revealCell(cellSW); 
+        cellSS.className==="cell cell-back" && revealCell(cellSS); 
+        cellSE.className==="cell cell-back" && revealCell(cellSE); 
+    };
+};
+
 function findValue(cell) {
     let x = cell.getAttribute("x");
     let y = cell.getAttribute("y");
@@ -282,6 +338,9 @@ function findValue(cell) {
 function revealCell(cell) {
     findValue(cell);
     unrevealed -= 1;
+    if (cell.classList=="cell cell-0"){
+        uncoverNeighbours(cell);
+    };
 };
 
 // ================
